c6f27476e2b307ae3e16620900fa8465baa983e5
manish.rayamajhi <manish.rayamajhi@igctech.com.np>
2019-04-08 14:48:48 +0545

    add

--- resources/views/logins.blade.php
+++ resources/views/logins.blade.php
@@ -116,10 +116,10 @@
     <!-- ============================================================== -->
     <!-- All Required js -->
     <!-- ============================================================== -->
-    <script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{asset('admin/assets/libs/jquery/dist/jquery.min.js')}}"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="../../assets/libs/popper.js/dist/umd/popper.min.js"></script>
-    <script src="../../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="{{asset('admin/assets/libs/popper.js/dist/umd/popper.min.')}}"></script>
+    <script src="{{asset('admin/assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <!-- ============================================================== -->
     <!-- This page plugin js -->
     <!-- ============================================================== -->
